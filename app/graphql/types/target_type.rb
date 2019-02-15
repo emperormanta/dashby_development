857203f8c8c834906defed1df2bc7d353aa0be27
Willy Budiman Tedjakusuma <willy.tedjakusuma@dwp.co.id>
2019-02-15 15:29:55 +0700

    Added User Name on Master Target

--- app/graphql/types/target_type.rb
+++ app/graphql/types/target_type.rb
@@ -1,9 +1,15 @@
 class Types::TargetType < GraphQL::Schema::Object
   field :user_id, String, null: true
+  field :user_name, String, null: true
   field :nominal, Integer, null: true
   field :hit_rate, Integer, null: true
   field :one_time, Integer, null: true
   field :periodic, Integer, null: true
   field :nominal_tolerance, Integer, null: false
   field :acquisition, Integer, null: false
+
+  def user_name
+    user = User.find_by(user_id: object.user_id)
+    return user.full_name
+  end
 end

6f1b6be20cce95f547c5f3156c74b18498642ae1
Kevin Goentoro <kevin.goentoro@dwp.co.id>
2019-02-18 15:42:14 +0700

    Change configuration for deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "xcost-be.dwp.io", user: "appsdwp", roles: %w{app db web}, my_property: :my_value
+server "trialdashby-be.dwp.io", user: "appsdwp", roles: %w{app db web}, my_property: :my_value
 
 
 

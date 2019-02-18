4583c8994ab842ada66f953278acc115c12b6dc4
Willy Budiman Tedjakusuma <willy.tedjakusuma@dwp.co.id>
2019-02-18 07:51:01 +0700

    Added user division in target type

--- app/graphql/types/target_type.rb
+++ app/graphql/types/target_type.rb
@@ -1,6 +1,7 @@
 class Types::TargetType < GraphQL::Schema::Object
   field :user_id, String, null: true
   field :user_name, String, null: true
+  field :user_division, String, null: true
   field :nominal, Integer, null: true
   field :hit_rate, Integer, null: true
   field :one_time, Integer, null: true
@@ -9,7 +10,17 @@ class Types::TargetType < GraphQL::Schema::Object
   field :acquisition, Integer, null: false
 
   def user_name
-    user = User.find_by(id: object.user_id)
-    return user.full_name
+    user_name = get_user(object.user_id).full_name
+    return user_name
+  end
+  
+  def user_division
+    user = get_user(object.user_id).division
+    return JSON.parse(user.to_str).first["abbreviation"]
+  end
+
+  private
+  def get_user(id)
+    return User.find_by(id: id)
   end
 end
